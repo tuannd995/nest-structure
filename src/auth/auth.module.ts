077d9c970e644b39dc9f556a64ce61e47a76aaac
duyhiep2519 <uyhiep2519@gmail.com>
2021-10-20 17:17:16 +0700

    change  expiresIn

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { LocalStrategy } from './strategies/local.strategy';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '60m' },
+        signOptions: { expiresIn: '7d' },
       }),
     }),
     UserModule,
