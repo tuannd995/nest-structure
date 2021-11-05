ccee74b84c0f433a358fc35130534ac71a64e415
duyhiep2519 <uyhiep2519@gmail.com>
2021-11-05 10:46:57 +0700

    add task list api

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Auth } from 'src/common/decorators/auth.decorator';
-import { Response, Role } from 'src/utils/types';
+import { Response, Role, TaskStatus } from 'src/utils/types';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './entities/task.entity';
 import { TaskService } from './task.service';
 
+export type TaskStatusAndSequence = {
+  status: TaskStatus;
+  sequence: number;
+};
 @Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
@@ -15,6 +28,52 @@ export class TaskController {
     const data = await this.taskService.create(createTaskDto);
     return {
       message: 'Task created successfully',
+      error: false,
+      data,
+    };
+  }
+
+  //get task with id
+  @Auth(Role.PM, Role.Member)
+  @Get('/:id')
+  async getTask(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Response<Task>> {
+    const data = await this.taskService.getTask(id);
+    return {
+      message: 'Get task  successfully',
+      error: false,
+      data,
+    };
+  }
+
+  // update task
+  @Auth(Role.PM, Role.Member)
+  @Put('/:id')
+  async updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Response<Task>> {
+    const data = await this.taskService.updateTask(id, updateTaskDto);
+    return {
+      message: 'Task updated successfully',
+      error: false,
+      data,
+    };
+  }
+  //update task status
+  @Auth(Role.PM, Role.Member)
+  @Put('/:id/status')
+  async updateTaskStatusAndSequence(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTask: TaskStatusAndSequence,
+  ): Promise<Response<Task>> {
+    const data = await this.taskService.updateTaskStatusAndSequence(
+      id,
+      updateTask,
+    );
+    return {
+      message: 'Task status updated successfully',
       error: false,
       data,
     };
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,3 +1,4 @@
+import { UpdateTaskDto } from './dto/update-task.dto';
 import {
   BadRequestException,
   Injectable,
@@ -9,6 +10,7 @@ import { Brackets, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskFilterDto } from './dto/filter.dto';
 import { Task } from './entities/task.entity';
+import { TaskStatusAndSequence } from './task.controller';
 
 @Injectable()
 export class TaskService {
@@ -17,6 +19,18 @@ export class TaskService {
     private readonly taskRepository: Repository<Task>,
   ) {}
 
+  // find tasks with status and projectId
+  async getTasksByStatusAndProjectId(status: TaskStatus, projectId: number) {
+    return await this.taskRepository.find({
+      where: {
+        status,
+        projectId,
+      },
+      order: {
+        sequence: 'DESC',
+      },
+    });
+  }
   // create new task
   async create(createTaskDto: CreateTaskDto) {
     const taskExits = await this.taskRepository.findOne({
@@ -34,15 +48,10 @@ export class TaskService {
     }
 
     // find tasks with the same status and projectId
-    const tasks = await this.taskRepository.find({
-      where: {
-        status: createTaskDto.status,
-        projectId: createTaskDto.projectId,
-      },
-      order: {
-        sequence: 'DESC',
-      },
-    });
+    const tasks = await this.getTasksByStatusAndProjectId(
+      createTaskDto.status,
+      createTaskDto.projectId,
+    );
 
     const sequence = tasks.length > 0 ? tasks[0].sequence + 1 : 1;
 
@@ -54,7 +63,7 @@ export class TaskService {
     return await this.taskRepository.save(newTask);
   }
 
-  // get list tasks
+  // get list tasks in project with project id
   async getTasksInProject(filterDto: TaskFilterDto, projectId: number) {
     const { statuses, keyword, priority, assignToId } = filterDto;
 
@@ -85,12 +94,124 @@ export class TaskService {
       );
     }
 
-    const tasks = await query.orderBy('task.sequence', 'DESC').getMany();
+    const tasks = await query.orderBy('task.sequence', 'ASC').getMany();
 
     if (!tasks || !tasks.length) {
       throw new NotFoundException('No tasks found in project');
     }
 
     return tasks;
+  }
+  // get task with id
+  async getTask(id: number) {
+    const task = await this.taskRepository.findOne({
+      where: {
+        id,
+      },
+      relations: ['assignTo'],
+    });
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return task;
+  }
+  // update task
+  async updateTask(id: number, updateTaskDto: UpdateTaskDto) {
+    const task = await this.taskRepository.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    const updatedTask = this.taskRepository.merge(task, updateTaskDto);
+
+    return await this.taskRepository.save(updatedTask);
+  }
+
+  // update task status
+  async updateTaskStatusAndSequence(
+    id: number,
+    updateTask: TaskStatusAndSequence,
+  ) {
+    const { status, sequence } = updateTask;
+
+    const currentTask = await this.taskRepository.findOne(id);
+
+    if (!currentTask) {
+      throw new NotFoundException('Task not found');
+    }
+
+    // find tasks with the same status and projectId
+    const tasksWithNewStatus = await this.getTasksByStatusAndProjectId(
+      status,
+      currentTask.projectId,
+    );
+
+    const tasksWithOldStatus = await this.getTasksByStatusAndProjectId(
+      currentTask.status,
+      currentTask.projectId,
+    );
+
+    // if task not change status
+    if (currentTask.status === status) {
+      // find task with sequence  = sequence
+      // move task to the bot of column
+      if (sequence === tasksWithOldStatus.length && currentTask.sequence == 1) {
+        tasksWithOldStatus.forEach((task) => {
+          task.sequence = task.sequence - 1;
+        });
+        const updatedTask = this.taskRepository.merge(currentTask, {
+          sequence,
+        });
+        await this.taskRepository.save(tasksWithOldStatus);
+        await this.taskRepository.save(updatedTask);
+        return await this.taskRepository.save(updatedTask);
+      } else {
+        const taskWithSequence = tasksWithOldStatus.find(
+          (t) => t.sequence === sequence,
+        );
+
+        if (taskWithSequence) {
+          taskWithSequence.sequence = currentTask.sequence;
+          await this.taskRepository.save(taskWithSequence);
+        }
+
+        const updatedTask = this.taskRepository.merge(currentTask, {
+          sequence,
+        });
+
+        return await this.taskRepository.save(updatedTask);
+      }
+      // if task change status
+    } else {
+      const updatedTask = this.taskRepository.merge(currentTask, {
+        status,
+        sequence,
+      });
+      //remove current task from tasks with old status
+
+      tasksWithOldStatus.length > 0 &&
+        tasksWithOldStatus.forEach(
+          (task) => task.sequence > 1 && (task.sequence = task.sequence - 1),
+        );
+      tasksWithOldStatus.splice(
+        tasksWithOldStatus.findIndex((t) => t.id === currentTask.id),
+        1,
+      );
+
+      await this.taskRepository.save(tasksWithOldStatus);
+
+      tasksWithNewStatus.length > 0 &&
+        tasksWithNewStatus.forEach((task) => {
+          if (task.sequence >= sequence) {
+            task.sequence = task.sequence + 1;
+          }
+        });
+      await this.taskRepository.save(tasksWithNewStatus);
+
+      return await this.taskRepository.save(updatedTask);
+    }
   }
 }
